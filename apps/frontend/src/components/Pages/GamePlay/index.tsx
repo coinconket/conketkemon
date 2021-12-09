37cd6c8d5a05a2d988d0297eda171c0309f20f0c
CoinConKet <coinconket@gmail.com>
2021-12-10 00:41:31 +0700

    add game play

--- apps/frontend/src/components/Pages/GamePlay/index.tsx
+++ apps/frontend/src/components/Pages/GamePlay/index.tsx
@@ -1,8 +1,19 @@
-import { Typography } from 'antd';
 import React from 'react';
 
 const GamePlay: React.FC = () => {
-  return <Typography>Game play page</Typography>;
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/L9-61i_hLII?start=15"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
 };
 
 export default GamePlay;
