f99430507a0f37fa5ff6617581cf1b5652735c06
CoinConKet <coinconket@gmail.com>
2021-12-12 01:07:49 +0700

    Add DEX

--- apps/frontend/src/components/Home/index.tsx
+++ apps/frontend/src/components/Home/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import BuyToken from '../Pages/BuyToken';
 import GamePlay from '../Pages/GamePlay';
 import ItemList from '../Pages/ItemList';
 import WalletButton from '../WalletButton';
@@ -17,6 +18,7 @@ const Home: React.FC = () => {
         <div>
           <StyledNavItem to="/pokemon">Pokemons</StyledNavItem>
           <StyledNavItem to="/gameplay">GamePlay</StyledNavItem>
+          <StyledNavItem to="/buy-token">Buy Token</StyledNavItem>
         </div>
         <div>
           <WalletButton />
@@ -25,6 +27,7 @@ const Home: React.FC = () => {
       <Routes>
         <Route path="/pokemon" element={<ItemList />} />
         <Route path="/gameplay" element={<GamePlay />} />
+        <Route path="/buy-token" element={<BuyToken />} />
         <Route path="*" element={<ItemList />} />
       </Routes>
     </div>
